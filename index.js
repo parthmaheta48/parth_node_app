6b3dfd6a947241ba87d5c4bfe8fdc6800be197c6
parthmaheta48 <parthkumar@ablespace.io>
2025-09-05 15:36:31 +0530

    fix: add welcome message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Parth Is Running on AWS App Runner Service !');
+    res.send('Welcome, Parth\'s App Is Running on AWS App Runner Service !');
 });
 
 app.get('/healthz', (req, res) => {
